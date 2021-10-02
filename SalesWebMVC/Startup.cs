38cfedbdd28701840dc39d353eccbc88efeb46f5
Isaque Schuwarte <isaque_schuwarte@hotmail.com>
2021-10-02 11:07:37 -0300

    Sellers Service and Basic Find All

--- SalesWebMVC/Startup.cs
+++ SalesWebMVC/Startup.cs
@@ -1,16 +1,11 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMVC.Data;
-using Microsoft.AspNetCore.Mvc;
+using SalesWebMVC.Services;
 
 namespace SalesWebMVC
 {
@@ -32,6 +27,7 @@ namespace SalesWebMVC
             services.AddControllersWithViews();
 
             services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
